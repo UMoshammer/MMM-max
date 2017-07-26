62e38c9b9b04e6e6e7b80d38e88e380fe8fc6831
Ulrich Moshammer <ulrich.moshammer@k-netz.at>
2017-07-26 15:57:46 +0200

    moved logic to node_helper.js, frontend displays room summary

--- MMM-max.js
+++ MMM-max.js
@@ -6,161 +6,130 @@
  * By mirko3000
  * MIT Licensed.
  */
-  var cache = [];
-  var cacheIndex = [];
 
 Module.register('MMM-max', {
 
-  requiresVersion: "2.0.0",
+    requiresVersion: "2.0.0",
 
-  defaults: {
-    fade: true,
-    fadePoint: 0.25,
-    maxIP: '192.168.0.252',
-    maxPort: 62910,
-    updateInterval: 5,
-    twoColLayout: false
-  },
-
-
-
-  // Override socket notification handler.
-  socketNotificationReceived: function (notification, payload) {
-      if (notification === 'MAX_DATA') {
-          Log.info('received MAX_DATA');
-          this.render(payload);
-          this.updateDom(3000);
-      }
-  },
-
-  start: function () {
-    Log.info("Starting module: " + this.name);
-    this.update();
-    // refresh every x minutes
-    setInterval(
-      this.update.bind(this),
-      this.config.updateInterval * 60 * 1000);
-  },
-
-  update: function(){
-    var maxConfig = {
-      maxIP: this.config.maxIP,
-      maxPort: this.config.maxPort
-    };
-
-    this.sendSocketNotification(
-      'MAX_UPDATE', maxConfig);
-  },
-
-  getScripts: function() {
-    return [
-      'String.format.js',
-      'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
-    ]
-  },
-
-  getStyles: function() {
-    return ['MMM-max.css'];
-  },
-
-  getDom: function() {
-    var content = '';
-    if (!this.loaded) {
-      content = this.html.loading;
-    }else { 
-      content = '<ul>'+this.dom+'</ul>';
-    }
-    return $('<div class="max">'+content+'</div>')[0];
-  },
+    defaults: {
+        fade: true,
+        fadePoint: 0.25,
+        maxIP: '192.168.0.252',
+        maxPort: 62910,
+        updateInterval: 5,
+        twoColLayout: false,
+        toDisplay: []
+    },
 
 
-  html: {
-    table: '<table>{0}</table>',
-    col: '<td align="left" class="normal light small">{0}</td><td align="left" class="dimmed light xsmall">{1}°C</td><td align="left" class="dimmed light xsmall">{2}°C</td><td align="left" class="dimmed light xsmall">{3}%</td><td class="dimmed xsmall light"><div class="fa fa-1 {4}"></div></td>',
-    row: '<tr>{0}{1}</tr>',
-    room: '<li><div class="room xsmall">{0} : {1}°C</div></li>',
-    loading: '<div class="dimmed light xsmall">Connecting to MAX! cube...</div>',
-  },
+    // Override socket notification handler.
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'MAX_DATA') {
+            Log.info('received MAX_DATA');
+            this.render(payload);
+            this.updateDom(3000);
+        }
+    },
 
-  render: function(data){
-      var previousCol =''
-      var rowCount = 0;
-      var tableText = ''
-      $.each(data, function (i, item) {
-        var room = {
-          id: item.rf_address,
-          name: item.deviceInfo.room_name,
-          temp: item.temp,
-          setpoint: item.setpoint,
-          valve: item.valve,
-          mode: item.mode
+    start: function () {
+        Log.info("Starting module: " + this.name);
+        this.update();
+        // refresh every x minutes
+        setInterval(
+            this.update.bind(this),
+            this.config.updateInterval * 60 * 1000);
+    },
+
+    update: function () {
+        var maxConfig = {
+            maxIP: this.config.maxIP,
+            maxPort: this.config.maxPort
         };
 
-        // Get previously cached entry - if exists
-        if (cacheIndex.indexOf(room.id) != -1) {
-          var cacheRoom = cache[cacheIndex.indexOf(room.id)];
+        this.sendSocketNotification(
+            'MAX_UPDATE', maxConfig);
+    },
 
-          // Check if temp is better from cache then current value
-          if (room.temp === 0) {
-            room.temp = cacheRoom.temp;
-          }
+    getScripts: function () {
+        return [
+            'String.format.js',
+            'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
+            'https://use.fontawesome.com/e32f11f60f.js' //TODO: replace with local Fontawsome files
+        ]
+    },
 
-          // Update cache
-          cache[cacheIndex.indexOf(room.id)] = room;
-        }
-        else {
-          // Create new entry in cache
-          cacheIndex[cacheIndex.length] = room.id;
-          cache[cache.length] = room;
-        }
+    getStyles: function () {
+        return ['MMM-max.css'];
+    },
 
-        // sometimes the temperature ist not given, initialize it with "-"
-        if (!room.temp) {
-          room.temp = '-';
-        }
-
-        var icon = "";
-        // Check for automatic or manual mode
-        if (room.mode === "VACATION") {
-          icon = "fa-plane";
-        }
-        else if (room.mode === "AUTO") {
-          icon = "fa-dashboard";
+    getDom: function () {
+        var content = '';
+        if (!this.loaded) {
+            content = this.html.loading;
         } else {
-          icon = "fa-hand-stop-o";
+            content = '<ul>' + this.dom + '</ul>';
+        }
+        return $('<div class="max">' + content + '</div>')[0];
+    },
+
+
+    html: {
+        table: '<table>{0}</table>',
+        col: '<td align="left" class="normal light small">{0}</td><td align="left" class="dimmed light xsmall">{1}°C</td><td align="left" class="dimmed light xsmall">{2}°C</td><td align="left" class="dimmed light xsmall">{3}%</td><td class="dimmed xsmall light"><div class="fa fa-1 {4}"></div></td>',
+        row: '<tr>{0}{1}</tr>',
+        room: '<li><div class="room xsmall">{0} : {1}°C</div></li>',
+        loading: '<div class="dimmed light xsmall">Connecting to MAX! cube...</div>'
+    },
+
+    render: function (data) {
+        var previousCol = '';
+        var tableText = '';
+        var rowCount = 0;
+        var currCols = [];
+
+        data.forEach(function (room) {
+            if (this.config.toDisplay.length === 0 || this.config.toDisplay.indexOf(room.name) !== 1) {
+                var icon = "";
+                // Check for automatic or manual mode
+                if (room.mode === "VACATION") {
+                    icon = "fa-plane";
+                }
+                else if (room.mode === "AUTO") {
+                    icon = "fa-dashboard";
+                } else {
+                    icon = "fa-hand-stop-o";
+                }
+                currCols.push(this.html.col.format(room.name, room.setpoint, room.temp, room.valve, icon));
+            }
+        }.bind(this));
+
+        for (var i = 0; i < currCols.length; i++) {
+            if (i % 2 !== 0 || !this.config.twoColLayout) {
+                // start new row
+                tableText += this.html.row.format(previousCol, currCols[i]);
+                previousCol = '';
+                //rowCount++;
+            }
+            else {
+                previousCol = currCols[i];
+            }
         }
 
-        var currCol = this.html.col.format(room.name, room.setpoint, room.temp, room.valve, icon);
-
-        if (i%2!=0 || !this.config.twoColLayout) {
-          // start new row
-          tableText += this.html.row.format(previousCol, currCol);
-          previousCol = '';
-          rowCount++;
-        }
-        else {
-          previousCol = currCol;
+        // Print last row if uneven
+        if (previousCol !== '') {
+            tableText += this.html.row.format(previousCol, '');
+            previousCol = '';
+            //rowCount++;
         }
 
-        //text += this.renderRoom(room, mode, temp, valve, time_until, locked);
-      }.bind(this));
+        text = this.html.table.format(tableText);
 
-      // Print last row if uneven
-      if (previousCol != '') {
-        tableText += this.html.row.format(previousCol, '');
-        previousCol = '';
-        rowCount++;
-      }
+        this.loaded = true;
 
-      text = this.html.table.format(tableText);
-
-      this.loaded = true;
-
-      // only update dom if content changed
-      if(this.dom !== text){
-        this.dom = text;
-        this.updateDom(this.config.animationSpeed);
-      }
-  },
-
+        // only update dom if content changed
+        if (this.dom !== text) {
+            this.dom = text;
+            this.updateDom(this.config.animationSpeed);
+        }
+    }
 });
--- node_helper.js
+++ node_helper.js
@@ -11,30 +11,79 @@ const NodeHelper = require('node_helper');
 
 var MaxCube = require('./maxcube/maxcube');
 
+var cache = [];
+var cacheIndex = [];
+
 module.exports = NodeHelper.create({
 
-  start: function () {
-    console.log("Starting max helper");
-  },
+    start: function () {
+        console.log("Starting max helper");
+    },
 
-  //Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'MAX_UPDATE') {
-      console.log("Triggering MAX upate");
-      var self = this;
-      var config = payload;
-      var myMaxCube  = new MaxCube(config.maxIP, config.maxPort);
-      myMaxCube.on('connected', function () {
+    //Subclass socketNotificationReceived received.
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'MAX_UPDATE') {
+            console.log("Triggering MAX update");
+            var self = this;
+            var config = payload;
+            var myMaxCube = new MaxCube(config.maxIP, config.maxPort);
+            myMaxCube.on('connected', function () {
 
-        myMaxCube.getDeviceStatus().then(function (devices) {
+                myMaxCube.getDeviceStatus().then(function (devices) {
+                    var rooms = [];
 
-          for (var i = 0; i < devices.length; i++) { 
-              devices[i].deviceInfo = myMaxCube.getDeviceInfo(devices[i].rf_address);
-          }
-          self.sendSocketNotification('MAX_DATA', devices);
-          myMaxCube.close();
-        });
-      });
+                    for (var i = 0; i < devices.length; i++) {
+                        var item = devices[i];
+                        item.deviceInfo = myMaxCube.getDeviceInfo(item.rf_address);
+                        var room;
+
+                        if (item.deviceInfo.room_name in rooms) {
+                            room = rooms[item.deviceInfo.room_name];
+                        } else {
+                            room = {
+                                name: item.deviceInfo.room_name,
+                                valve: 0
+                            };
+                        }
+
+                        switch (item.deviceInfo.device_type) {
+                            case 1:
+                                room.valve = (room.valve + item.valve) / 2;
+                                if (item.setpoint !== 0)
+                                    room.setpoint = item.setpoint;
+                                room.mode = item.mode;
+                                break;
+                            case 2:
+                                room.valve = (room.valve + item.valve) / 2;
+                                if (item.setpoint !== 0)
+                                    room.setpoint = item.setpoint;
+                                room.mode = item.mode;
+                                break;
+                            case 3:
+                                if (item.temp !== 0)
+                                    room.temp = item.temp;
+                                if (item.setpoint !== 0)
+                                    room.setpoint = item.setpoint;
+                                room.mode = item.mode;
+                                break;
+                            case 4:
+                                break;
+                        }
+
+                        // sometimes the temperature ist not given, initialize it with "-"
+                        if (!room.temp) {
+                            room.temp = -1;
+                        }
+                        rooms[room.name] = room;
+                    }
+                    var transfer_rooms = []; //sendSocketNotification can not handle associated arrays
+                    for(room in rooms) {
+                        transfer_rooms.push(rooms[room]);
+                    }
+                    self.sendSocketNotification('MAX_DATA', transfer_rooms);
+                    myMaxCube.close();
+                });
+            });
+        }
     }
-  }
 });
